b799a005b44691dc144f50f80a364b4c2ad1b99d
knnorbi <kluka.norbert@gmail.com>
2018-09-25 14:15:03 +0200

    Initial commit

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Meddig számoljak? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FIZZBUZZ");
+                else if(i % 3 == 0)
+                    Console.WriteLine("FIZZ");
+                else if(i % 5 == 0)
+                    Console.WriteLine("BUZZ");
+                else
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
